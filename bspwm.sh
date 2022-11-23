c7ab7a4d8307946de3b62c9c0501ab893ef7386b
AlexanderR <96749659+Nazeofel@users.noreply.github.com>
2022-11-23 19:04:47 +0100

    change
    change

--- bspwm.sh
+++ bspwm.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu --needed git base-devel
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -si
-yay -S bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey adobe-source-code-pro-fonts nerd-fonts-jetbrains-mono ttf-iosevka-nerd polkit-gnome i3lock-color fortune-mod network-manager-applet zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+yay -S bspwm sxhkd feh polybar xorg-xsetroot picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey adobe-source-code-pro-fonts nerd-fonts-jetbrains-mono ttf-iosevka-nerd polkit-gnome i3lock-color fortune-mod network-manager-applet zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
 git clone https://github.com/nazeofel/BSPWM-FILES.git --depth 1
 cd BSPWM-FILES
 cp -R .config/* ~/.config/
